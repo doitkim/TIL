b5b64ab79a1ccb6740ab52911ebe009300eacc65
Kim Seoung Hwa <kimsh27214@gmail.com>
2023-06-29 13:46:31 +0900

    Delete angular/my-first-app directory
    recommit

--- angular/my-first-app/src/app/success-alert/success-alert.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-success-alert',
-  template: ` <p>자기야 성공했어</p> `,
-  styleUrls: ['./success-alert.component.css'],
-})
-export class SuccessAlertComponent {}
